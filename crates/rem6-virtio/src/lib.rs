1f778cfe0a67d2b2639332e6241c4de8711c3fea
Sihao Liu <sihao@cs.ucla.edu>
2026-05-26 00:46:56 -0700

    virtio: split root reexports

--- crates/rem6-virtio/src/lib.rs
+++ crates/rem6-virtio/src/lib.rs
@@ -12,69 +12,13 @@ use rem6_mmio::{
 mod block;
 mod block_queue;
 mod device_config;
+mod exports;
 mod isr;
 mod pci_capability;
 mod shared_memory;
 mod transport;
 
-pub use block::{
-    VirtioBlockCacheMode, VirtioBlockCompletion, VirtioBlockConfigSpec, VirtioBlockDevice,
-    VirtioBlockDiscardLimits, VirtioBlockGeometry, VirtioBlockMemoryBackend, VirtioBlockRequest,
-    VirtioBlockRequestId, VirtioBlockRequestKind, VirtioBlockSecureEraseLimits, VirtioBlockStatus,
-    VirtioBlockTopology, VirtioBlockWriteZeroesLimits, VIRTIO_BLOCK_CONFIG_ALIGNMENT_OFFSET_OFFSET,
-    VIRTIO_BLOCK_CONFIG_BLK_SIZE_OFFSET, VIRTIO_BLOCK_CONFIG_CAPACITY_OFFSET,
-    VIRTIO_BLOCK_CONFIG_CYLINDERS_OFFSET, VIRTIO_BLOCK_CONFIG_DISCARD_ALIGNMENT_OFFSET,
-    VIRTIO_BLOCK_CONFIG_HEADS_OFFSET, VIRTIO_BLOCK_CONFIG_MAX_DISCARD_SECTORS_OFFSET,
-    VIRTIO_BLOCK_CONFIG_MAX_DISCARD_SEG_OFFSET,
-    VIRTIO_BLOCK_CONFIG_MAX_SECURE_ERASE_SECTORS_OFFSET,
-    VIRTIO_BLOCK_CONFIG_MAX_SECURE_ERASE_SEG_OFFSET,
-    VIRTIO_BLOCK_CONFIG_MAX_WRITE_ZEROES_SECTORS_OFFSET,
-    VIRTIO_BLOCK_CONFIG_MAX_WRITE_ZEROES_SEG_OFFSET, VIRTIO_BLOCK_CONFIG_MIN_IO_SIZE_OFFSET,
-    VIRTIO_BLOCK_CONFIG_NUM_QUEUES_OFFSET, VIRTIO_BLOCK_CONFIG_OPT_IO_SIZE_OFFSET,
-    VIRTIO_BLOCK_CONFIG_PHYSICAL_BLOCK_EXP_OFFSET, VIRTIO_BLOCK_CONFIG_SECTORS_OFFSET,
-    VIRTIO_BLOCK_CONFIG_SECURE_ERASE_ALIGNMENT_OFFSET, VIRTIO_BLOCK_CONFIG_SEG_MAX_OFFSET,
-    VIRTIO_BLOCK_CONFIG_SIZE, VIRTIO_BLOCK_CONFIG_SIZE_MAX_OFFSET,
-    VIRTIO_BLOCK_CONFIG_UNUSED0_OFFSET, VIRTIO_BLOCK_CONFIG_UNUSED1_OFFSET,
-    VIRTIO_BLOCK_CONFIG_WRITEBACK_OFFSET, VIRTIO_BLOCK_CONFIG_WRITE_ZEROES_MAY_UNMAP_OFFSET,
-    VIRTIO_BLOCK_DEVICE_ID, VIRTIO_BLOCK_F_BLK_SIZE, VIRTIO_BLOCK_F_CONFIG_WCE,
-    VIRTIO_BLOCK_F_DISCARD, VIRTIO_BLOCK_F_FLUSH, VIRTIO_BLOCK_F_GEOMETRY, VIRTIO_BLOCK_F_MQ,
-    VIRTIO_BLOCK_F_RO, VIRTIO_BLOCK_F_SECURE_ERASE, VIRTIO_BLOCK_F_SEG_MAX,
-    VIRTIO_BLOCK_F_SIZE_MAX, VIRTIO_BLOCK_F_TOPOLOGY, VIRTIO_BLOCK_F_WRITE_ZEROES,
-    VIRTIO_BLOCK_SECTOR_SIZE, VIRTIO_BLOCK_S_IOERR, VIRTIO_BLOCK_S_OK, VIRTIO_BLOCK_S_UNSUPP,
-    VIRTIO_BLOCK_T_DISCARD, VIRTIO_BLOCK_T_FLUSH, VIRTIO_BLOCK_T_GET_ID,
-    VIRTIO_BLOCK_T_GET_LIFETIME, VIRTIO_BLOCK_T_IN, VIRTIO_BLOCK_T_OUT,
-    VIRTIO_BLOCK_T_SECURE_ERASE, VIRTIO_BLOCK_T_WRITE_ZEROES,
-};
-pub use block_queue::{
-    VirtioBlockDecodedRequest, VirtioBlockDescriptorWrite, VirtioBlockInterruptCompletion,
-    VirtioBlockIntxCompletionTarget, VirtioBlockMsiCompletionTarget,
-    VirtioBlockMsixCompletionTarget, VirtioBlockQueueCompletionWrite, VirtioGuestMemory,
-    VirtioSplitDescriptor, VirtioSplitDescriptorChain, VirtioSplitQueue, VirtioSplitUsedElement,
-    VirtioSplitUsedRing, VIRTIO_SPLIT_DESC_F_INDIRECT, VIRTIO_SPLIT_DESC_F_NEXT,
-    VIRTIO_SPLIT_DESC_F_WRITE,
-};
-pub use device_config::{
-    VirtioPciDeviceConfigAccess, VirtioPciDeviceConfigDevice, VirtioPciDeviceConfigSnapshot,
-    VirtioPciDeviceConfigSpec,
-};
-pub use isr::{
-    VirtioPciIsrDevice, VirtioPciIsrEvent, VirtioPciIsrEventKind, VirtioPciIsrSnapshot,
-    VirtioPciIsrStatus, VIRTIO_PCI_ISR_STATUS_SIZE,
-};
-pub use pci_capability::{
-    VirtioPciBarIndex, VirtioPciCapabilityEntry, VirtioPciCapabilityKind,
-    VirtioPciCapabilityOffset, VirtioPciNotifyCapabilityEntry,
-};
-pub use shared_memory::{
-    VirtioPciSharedMemoryCap64Fields, VirtioPciSharedMemoryCapabilities,
-    VirtioPciSharedMemoryCapabilityEntry, VirtioPciSharedMemoryId, VirtioPciSharedMemoryRegion,
-    VirtioPciSharedMemoryRegionSpec, VirtioPciSharedMemoryRegistry,
-};
-pub use transport::{
-    VirtioPciModernTransportDevices, VirtioPciModernTransportSpec, VirtioPciNotifyRegion,
-    VirtioPciTransportBarRuntime, VirtioPciTransportBarSpec, VirtioPciTransportEndpointSpec,
-    VirtioPciTransportRegion,
-};
+pub use exports::*;
 
 pub const VIRTIO_PCI_COMMON_CONFIG_SIZE: u64 = 0x40;
 pub const VIRTIO_PCI_DEVICE_FEATURE_SELECT_OFFSET: u64 = 0x00;
