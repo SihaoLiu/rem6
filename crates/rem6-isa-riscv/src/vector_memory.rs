8598c806b64fe502223897b9266ddf254258a82e
Sihao Liu <sihao@cs.ucla.edu>
2026-07-02 07:05:38 -0400

    Support e8 indexed RVV memory with e64 indices

--- crates/rem6-isa-riscv/src/vector_memory.rs
+++ crates/rem6-isa-riscv/src/vector_memory.rs
@@ -19,6 +19,7 @@ const SUPPORTED_INDEXED_M1_SHAPES: &[(MemoryWidth, MemoryWidth, &[usize], usize)
     (MemoryWidth::Byte, MemoryWidth::Byte, &[0, 15], 16),
     (MemoryWidth::Byte, MemoryWidth::Halfword, &[0, 15], 16),
     (MemoryWidth::Byte, MemoryWidth::Word, &[0, 15], 16),
+    (MemoryWidth::Byte, MemoryWidth::Doubleword, &[0, 15], 16),
     (MemoryWidth::Halfword, MemoryWidth::Halfword, &[0, 14], 16),
     (MemoryWidth::Halfword, MemoryWidth::Word, &[0, 14], 16),
     (MemoryWidth::Halfword, MemoryWidth::Doubleword, &[0, 14], 16),
